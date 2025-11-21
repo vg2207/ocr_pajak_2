b2ccdfba0b12d1018d825a34c3f7043af728ac51
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-11-21 16:05:34 +0700

    Update ocr_pajak_2.py

--- ocr_pajak_2.py
+++ ocr_pajak_2.py
@@ -108,9 +108,9 @@ if user_input_excel is not None:
                 a = [extracted_text]
                 # st.write(a)
 
-                extracted_text_ntpn = re.findall('(?<=NTPN : )[^ ].*', a[0])
-                extracted_text_no_dokumen = re.findall('(?<=NOMOR DOKUMEN : )[^ ].*', a[0])
-                extracted_text_jumlah_setoran = re.findall('(?<=JUMLAH SETORAN : )[^ ].*', a[0])
+                extracted_text_ntpn = re.findall('(?<=NTPN : )[^ ]+(?=\s)', a[0])
+                extracted_text_no_dokumen = re.findall('(?<=NOMOR DOKUMEN : )[^ ]+(?=\s)', a[0])
+                extracted_text_jumlah_setoran = re.findall('(?<=JUMLAH SETORAN : )[^ ]+(?=\s)', a[0])
 
                 st.write(extracted_text_ntpn)
                 st.write(extracted_text_no_dokumen)
