7a6f87631a77df49d2ee1e44e9b55e56439ca359
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-11-21 16:31:47 +0700

    Update ocr_pajak_2.py

--- ocr_pajak_2.py
+++ ocr_pajak_2.py
@@ -73,55 +73,76 @@ if user_input_excel is not None:
 
                 extracted_text_billing_pdf = ""
 
-                with pdfplumber.open(path_to_pdf_billing) as pdf:
-                    for page in pdf.pages:
-                        # Use a corrected bounding box (x0, y0, x1, y1)
-                        # (left, bottom, right, top)
-                        cropped = page.within_bbox((int(0/20.35*595), int((3.5)/20.35*595), int(15/20.35*595), int((4.5)/20.35*595)))
-                        text = cropped.extract_text()
-                        if text:
-                            extracted_text_billing_pdf += text.strip()
+                try :
+                    with pdfplumber.open(path_to_pdf_billing) as pdf:
+                        for page in pdf.pages:
+                            # Use a corrected bounding box (x0, y0, x1, y1)
+                            # (left, bottom, right, top)
+                            cropped = page.within_bbox((int(0/20.35*595), int((3.5)/20.35*595), int(15/20.35*595), int((4.5)/20.35*595)))
+                            text = cropped.extract_text()
+                            if text:
+                                extracted_text_billing_pdf += text.strip()
+    
+                    extracted_text_no_billing = re.findall('(?<=Nomor Billing : )[^ ].*', extracted_text_billing_pdf)
+                    extracted_text_tanggal = re.findall('(?<=Tanggal : )[^ ].*', extracted_text_billing_pdf)
+    
+                    # st.write(extracted_text_no_billing)
+                    # st.write(extracted_text_tanggal)
+                    
+                    df['NO BILLING'][i] = str(extracted_text_no_billing[0])
+                    df['TANGGAL'][i] = str(extracted_text_tanggal[0])
 
-                extracted_text_no_billing = re.findall('(?<=Nomor Billing : )[^ ].*', extracted_text_billing_pdf)
-                extracted_text_tanggal = re.findall('(?<=Tanggal : )[^ ].*', extracted_text_billing_pdf)
+                except:
+                    df['NO BILLING'][i] = ''
+                    df['TANGGAL'][i] = ''
+    
+    
+                try :
+                    pattern_nama_file_BPN = 'BPN_' + str(extracted_text_no_billing[0]) + '*.pdf'
+                    path_to_pdf_BPN = os.path.join(path_to_BPN_folder, os.path.splitext(user_input_folder_bpn.name)[0], pattern_nama_file_BPN)
+                    
+                    path_to_pdf_BPN = glob.glob(str(path_to_pdf_BPN))
+                    # st.write(path_to_pdf_BPN)
+     
+                    reader_BPN = PdfReader(path_to_pdf_BPN[0])
+                    a=[]
+                    # Iterate through pages and extract text
+                    extracted_text = ""
+                    for page in reader_BPN.pages:
+                        extracted_text += page.extract_text()
+                    a = [extracted_text]
+                    # st.write(a)
+    
+                    extracted_text_ntpn = re.findall('(?<=NTPN : )[^ ].*', a[0])
+                    extracted_text_no_dokumen = re.findall('(?<=NOMOR DOKUMEN : )[^ ]+(?=\s)', a[0])
+                    extracted_text_jumlah_setoran = re.findall('(?<=JUMLAH SETORAN : )[^ ]+(?=\s)', a[0])
+    
+                    # st.write(extracted_text_ntpn)
+                    # st.write(extracted_text_no_dokumen)
+                    # st.write(extracted_text_jumlah_setoran)
+    
+                    df['NTPN'][i] = str(extracted_text_ntpn[0])
+                    df['NOMOR DOKUMEN'][i] = str(extracted_text_no_dokumen[0])
+                    df['JUMLAH SETORAN'][i] = str(extracted_text_jumlah_setoran[0])
 
-                # st.write(extracted_text_no_billing)
-                # st.write(extracted_text_tanggal)
-                
-                df['NO BILLING'][i] = str(extracted_text_no_billing[0])
-                df['TANGGAL'][i] = str(extracted_text_tanggal[0])
-
-
-
-                pattern_nama_file_BPN = 'BPN_' + str(extracted_text_no_billing[0]) + '*.pdf'
-                path_to_pdf_BPN = os.path.join(path_to_BPN_folder, os.path.splitext(user_input_folder_bpn.name)[0], pattern_nama_file_BPN)
-                
-                path_to_pdf_BPN = glob.glob(str(path_to_pdf_BPN))
-                # st.write(path_to_pdf_BPN)
- 
-                reader_BPN = PdfReader(path_to_pdf_BPN[0])
-                a=[]
-                # Iterate through pages and extract text
-                extracted_text = ""
-                for page in reader_BPN.pages:
-                    extracted_text += page.extract_text()
-                a = [extracted_text]
-                # st.write(a)
-
-                extracted_text_ntpn = re.findall('(?<=NTPN : )[^ ].*', a[0])
-                extracted_text_no_dokumen = re.findall('(?<=NOMOR DOKUMEN : )[^ ]+(?=\s)', a[0])
-                extracted_text_jumlah_setoran = re.findall('(?<=JUMLAH SETORAN : )[^ ]+(?=\s)', a[0])
-
-                # st.write(extracted_text_ntpn)
-                # st.write(extracted_text_no_dokumen)
-                # st.write(extracted_text_jumlah_setoran)
-
-                df['NTPN'][i] = str(extracted_text_ntpn[0])
-                df['NOMOR DOKUMEN'][i] = str(extracted_text_no_dokumen[0])
-                df['JUMLAH SETORAN'][i] = str(extracted_text_jumlah_setoran[0])
+                except :
+                    df['NTPN'][i] = ''
+                    df['NOMOR DOKUMEN'][i] = ''
+                    df['JUMLAH SETORAN'][i] = ''
+                    
                 
 
             st.write(df)
+
+            with st.spinner("Preparing for data to be downloaded ..."):
+                output = BytesIO()
+    
+                with pd.ExcelWriter(output, engine='xlsxwriter') as writer: 
+                    df_download = df.to_excel(writer)
+
+                output_excel_file_name = 'result' + ' ' + str(os.path.splitext(user_input_excel.name)[0]) + '.xlsx'
+        
+                button_clicked = st.download_button(label=':cloud: Download result', type="secondary", data=output.getvalue(),file_name=output_excel_file_name)
 
             
                 
